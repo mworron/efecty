f670c9351c150a7025980c8473c435f264a180b6
Jorge Vargas <jorgevargaslarrota@hotmail.com>
2014-05-05 16:47:14 -0500

    [*] MO: Efecty 1.6 version
    Add compatibility to 1.6 version

--- efecty/efecty.php
+++ efecty/efecty.php
@@ -1,5 +1,5 @@
 <?php
-/*
+/**
 * 2007-2011 PrestaShop 
 *
 * NOTICE OF LICENSE
@@ -18,10 +18,10 @@
 * versions in the future. If you wish to customize PrestaShop for your
 * needs please refer to http://www.prestashop.com for more information.
 *
-*  @author PrestaShop SA <contact@prestashop.com>
-*  @copyright  2007-2011 PrestaShop SA
-*  @version  Release: $Revision: 8005 $
-*  @license    http://opensource.org/licenses/afl-3.0.php  Academic Free License (AFL 3.0)
+*  @author       PrestaShop SA <contact@prestashop.com>
+*  @copyright    2007-2014 PrestaShop SA
+*  @version      Release: $Revision: 8005 $
+*  @license      http://opensource.org/licenses/afl-3.0.php  Academic Free License (AFL 3.0)
 *  International Registered Trademark & Property of PrestaShop SA
 */
 
@@ -33,30 +33,41 @@ class Efecty extends PaymentModule
 	private $_html = '';
 	private $_postErrors = array();
 
-	public  $details;
-	public  $owner;
+	public $efecty_name;
+	public $efecty_details;
+	public $extra_mail_vars;
 
 	public function __construct()
 	{
 		$this->name = 'efecty';
 		$this->tab = 'payments_gateways';
-		$this->version = '0.5';
-		$this->author = 'eTiendas.co';
-		
+		$this->version = '0.1';
+		$this->author = 'jorgevrgs';
 
-		$config = Configuration::getMultiple(array('EFECTY_WIRE_DETAILS', 'EFECTY_WIRE_OWNER'));
-		if (isset($config['EFECTY_WIRE_OWNER']))
-			$this->owner = $config['EFECTY_WIRE_OWNER'];
-		if (isset($config['EFECTY_WIRE_DETAILS']))
-			$this->details = $config['EFECTY_WIRE_DETAILS'];
+		$config = Configuration::getMultiple(array('EFECTY_DETAILS', 'EFECTY_NAME'));
+		if (isset($config['EFECTY_NAME']))
+			$this->efecty_name = $config['EFECTY_NAME'];
+		if (isset($config['EFECTY_DETAILS']))
+			$this->efecty_details = $config['EFECTY_DETAILS']
+
+		$this->bootstrap = true;
 
 		parent::__construct();
 
 		$this->displayName = $this->l('Efecty');
 		$this->description = $this->l('Accept payments using Efecty.');
 		$this->confirmUninstall = $this->l('Are you sure you want to delete your details?');
-		if (!isset($this->owner) OR !isset($this->details))
-			$this->warning = $this->l('Account owner and details must be configured in order to use this module correctly.');
+
+		if ((!isset($this->efecty_name) || !isset($this->efecty_details) || empty($this->efecty_name) || empty($this->efecty_details)))
+			$this->warning = $this->l('The "Account owner" and "Details" fields must be configured before using this module.');
+		if (!count(Currency::checkPaymentCurrencies($this->id)))
+			$this->warning = $this->l('No currency has been set for this module.');
+	
+		$this->extra_mail_vars = array(
+			'{efecty_name}' => Configuration::get('EFECTY_NAME'),
+			'{efecty_details}' => Configuration::get('EFECTY_DETAILS'),
+			'{efecty_details_html}' => str_replace("\n", '<br />', Configuration::get('EFECTY_DETAILS'))
+		);
 	}
 
 	public function install()
@@ -67,25 +78,96 @@ class Efecty extends PaymentModule
 		return true;
 	}
 
+	private function _createOrderState()
+	{
+		if (!Configuration::get('PS_OS_EFECTY'))
+		{
+			$orderState = new OrderState();
+			$orderState->name = array();
+
+			foreach (Language::getLanguages() as $language)
+			{
+				if (strtolower($language['iso_code']) == 'es')
+					$orderState->name[$language['id_lang']] = 'Efecty: Esperando pago';
+				else
+					$orderState->name[$language['id_lang']] = 'Efecty: Awaiting for payment';
+			}
+
+			$orderState->send_email = true;
+			$orderState->color = 'royalblue';
+			$orderState->hidden = false;
+			$orderState->delivery = false;
+			$orderState->logable = false;
+			$orderState->invoice = false;
+			$orderState->template = 'efecty';
+			if ($orderState->add())
+			{
+				@copy(dirname(__FILE__).'/../../modules/'.$this->name.'/logo.jpg', dirname(__FILE__).'/../../img/os/'.(int)$orderState->id.'.gif');
+				@copy(dirname(__FILE__).'/../../modules/'.$this->name.'/copymails/en/efecty.html', dirname(__FILE__).'/../../mails/en/efecty.html');
+				@copy(dirname(__FILE__).'/../../modules/'.$this->name.'/copymails/en/efecty.txt', dirname(__FILE__).'/../../mails/en/efecty.txt');
+				@copy(dirname(__FILE__).'/../../modules/'.$this->name.'/copymails/es/efecty.html', dirname(__FILE__).'/../../mails/es/efecty.html');
+				@copy(dirname(__FILE__).'/../../modules/'.$this->name.'/copymails/es/efecty.txt', dirname(__FILE__).'/../../mails/es/efecty.txt');
+				Configuration::updateValue('PS_OS_EFECTY', (int)$orderState->id);
+			}
+		}
+	}
+
 	public function uninstall()
 	{
-		if (!Configuration::deleteByName('EFECTY_WIRE_DETAILS')
-				OR !Configuration::deleteByName('EFECTY_WIRE_OWNER')
-				OR !parent::uninstall())
+		if (!Configuration::deleteByName('EFECTY_DETAILS')
+		|| !Configuration::deleteByName('EFECTY_NAME')
+		|| !parent::uninstall())
 			return false;
 		if ($this->_deleteOrderState())
 			Configuration::deleteByName('PS_OS_EFECTY');
 		$this->_deleteMails();
 		return true;
 	}
+	
+	private function _deleteOrderState()
+	{
+		$orderState = new OrderState(Configuration::get('PS_OS_EFECTY'));
+		return $orderState->delete();	
+	}
+	
+	private function _deleteMails()
+	{
+		@unlink(dirname(__FILE__).'/../../mails/en/efecty.html');
+		@unlink(dirname(__FILE__).'/../../mails/en/efecty.txt');
+		@unlink(dirname(__FILE__).'/../../mails/es/efecty.html');
+		@unlink(dirname(__FILE__).'/../../mails/es/efecty.txt');
+	}
+
+	public function getContent()
+	{
+		if (Tools::isSubmit('btnSubmit'))
+		{
+			$this->_postValidation();
+			if (!count($this->_postErrors))
+				$this->_postProcess();
+			else
+				foreach ($this->_postErrors as $err)
+					$this->_html .= $this->displayError($err);
+		}
+
+		$this->_html .= $this->_displayEfecty();
+		$this->_html .= $this->renderForm();
+
+		return $this->_html;
+	}
+
+	private function _displayEfecty()
+	{
+		return $this->display(__FILE__, 'infos.tpl');
+	}
 
 	private function _postValidation()
 	{
 		if (Tools::isSubmit('btnSubmit'))
 		{
-			if (!Tools::getValue('details'))
+			if (!Tools::getValue('EFECTY_DETAILS'))
 				$this->_postErrors[] = $this->l('Account details are required.');
-			elseif (!Tools::getValue('owner'))
+			elseif (!Tools::getValue('EFECTY_NAME'))
 				$this->_postErrors[] = $this->l('Account owner is required.');
 		}
 	}
@@ -94,144 +176,66 @@ class Efecty extends PaymentModule
 	{
 		if (Tools::isSubmit('btnSubmit'))
 		{
-			Configuration::updateValue('EFECTY_WIRE_DETAILS', Tools::getValue('details'));
-			Configuration::updateValue('EFECTY_WIRE_OWNER', Tools::getValue('owner'));
+			Configuration::updateValue('EFECTY_NAME', pSQL(Tools::getValue('EFECTY_NAME')));
+			Configuration::updateValue('EFECTY_DETAILS', pSQL(Tools::getValue('EFECTY_DETAILS')));
 		}
-		$this->_html .= '<div class="conf confirm"><img src="../img/admin/ok.gif" alt="'.$this->l('ok').'" /> '.$this->l('Settings updated').'</div>';
+		$this->_html .= $this->displayConfirmation($this->l('Settings updated');
 	}
 
-	private function _displayEfecty()
+	private function _renderForm()
 	{
-		$this->_html .= '<img src="../modules/'.$this->name.'/efecty.jpg" width="86" height="49" style="float:left; margin-right:15px;"><b>'.$this->l('This module allows you to accept payments using Efecty.').'</b><br /><br />
-		'.$this->l('If the client chooses this payment mode, the order will change its status into a \'Waiting for payment\' status.').'<br />
-		'.$this->l('Therefore, you must manually confirm the order as soon as you receive the payment.').'<br /><br /><br />';
+		$fields_form = array(
+			'form' => array(
+				'legend' => array(
+					'title' => $this->l('Contact details'),
+					'icon' => 'icon-envelope'
+				),
+				'input' => array(
+					array(
+						'type' => 'text',
+						'label' => $this->l('Account name'),
+						'name' => 'EFECTY_NAME',
+					),
+					array(
+						'type' => 'textarea',
+						'label' => $this->l('Details'),
+						'desc' => $this->l('Set your Efecty details like CC or NIT, phone,...'),
+						'name' => 'EFECTY_DETAILS',
+					),
+				),
+				'submit' => array(
+					'title' => $this->l('Save'),
+				)
+			),
+		);
+		
+		$helper = new HelperForm();
+		$helper->show_toolbar = false;
+		$helper->table =  $this->table;
+		$lang = new Language((int)Configuration::get('PS_LANG_DEFAULT'));
+		$helper->default_form_language = $lang->id;
+		$helper->allow_employee_form_lang = Configuration::get('PS_BO_ALLOW_EMPLOYEE_FORM_LANG') ? Configuration::get('PS_BO_ALLOW_EMPLOYEE_FORM_LANG') : 0;
+		$this->fields_form = array();
+		$helper->id = (int)Tools::getValue('id_carrier');
+		$helper->identifier = $this->identifier;
+		$helper->submit_action = 'btnSubmit';
+		$helper->currentIndex = $this->context->link->getAdminLink('AdminModules', false).'&configure='.$this->name.'&tab_module='.$this->tab.'&module_name='.$this->name;
+		$helper->token = Tools::getAdminTokenLite('AdminModules');
+		$helper->tpl_vars = array(
+			'fields_value' => $this->getConfigFieldsValues(),
+			'languages' => $this->context->controller->getLanguages(),
+			'id_language' => $this->context->language->id
+		);
+
+		return $helper->generateForm(array($fields_form));
 	}
 
-	private function _displayForm()
+	public function getConfigFieldsValues()
 	{
-		$this->_html .=
-		'<form action="'.Tools::htmlentitiesUTF8($_SERVER['REQUEST_URI']).'" method="post">
-			<fieldset>
-			<legend><img src="../img/admin/contact.gif" />'.$this->l('Efecty Details for Order of Service ').'</legend>
-				<table border="0" width="500" cellpadding="0" cellspacing="0" id="form">
-					<tr><td colspan="2">'.$this->l('Please specify the Efecty account details for customers').'.<br /><br /></td></tr>
-					<tr><td width="130" style="height: 35px;">'.$this->l('Account owner').'</td><td><input type="text" name="owner" value="'.htmlentities(Tools::getValue('owner', $this->owner), ENT_COMPAT, 'UTF-8').'" style="width: 300px;" /></td></tr>
-					<tr>
-						<td width="130" style="vertical-align: top;">'.$this->l('Your Account Code:').'</td>
-						<td style="padding-bottom:15px;">
-							<textarea name="details" rows="4" cols="53">'.htmlentities(Tools::getValue('details', $this->details), ENT_COMPAT, 'UTF-8').'</textarea>
-							<p>'.$this->l('NIT, Cedula, etc.').'</p>
-						</td>
-					</tr>
-					<tr><td colspan="2" align="center"><input class="button" name="btnSubmit" value="'.$this->l('Update settings').'" type="submit" /></td></tr>
-				</table>
-			</fieldset>
-		</form>';
-	}
-
-	public function getContent()
-	{
-		$this->_html = '<h2>'.$this->displayName.'</h2>';
-
-		if (Tools::isSubmit('btnSubmit'))
-		{
-			$this->_postValidation();
-			if (!sizeof($this->_postErrors))
-				$this->_postProcess();
-			else
-				foreach ($this->_postErrors AS $err)
-					$this->_html .= '<div class="alert error">'. $err .'</div>';
-		}
-		else
-			$this->_html .= '<br />';
-
-		$this->_displayEfecty();
-		$this->_displayForm();
-
-		return $this->_html;
-	}
-
-	public function execPayment($cart)
-	{
-		if (!$this->active)
-			return ;
-		if (!$this->checkCurrency($cart))
-			Tools::redirectLink(__PS_BASE_URI__.'order.php');
-			global $cookie, $smarty;
-	
-			$smarty->assign(array(
-				'nbProducts' => $cart->nbProducts(),
-				'total' => $cart->getOrderTotal(true, Cart::BOTH),
-				'this_path' => $this->_path,
-				'this_path_ssl' => Tools::getShopDomainSsl(true, true).__PS_BASE_URI__.'modules/'.$this->name.'/'
-			));
-
-		return $this->display(__FILE__, 'payment_execution.tpl');
-	}
-
-	public function hookPayment($params)
-	{
-		if (!$this->active)
-			return ;
-		if (!$this->checkCurrency($params['cart']))
-			return ;
-
-		if (substr(_PS_VERSION_, 0, 3) == '1.4')
-		{
-			global $smarty;
-			$smarty->assign(array(
-				'this_path' => $this->_path,
-				'this_path_ssl' => Tools::getShopDomainSsl(true, true).__PS_BASE_URI__.'modules/'.$this->name.'/'
-			));
-		}
-		if (substr(_PS_VERSION_, 0, 3) == '1.5')
-		{
-			$this->smarty->assign(array(
-				'this_path' => $this->_path,
-				'this_path_ssl' => Tools::getShopDomainSsl(true, true).__PS_BASE_URI__.'modules/'.$this->name.'/'
-			));
-		}
-		return $this->display(__FILE__, 'payment.tpl');
-	}
-
-	public function hookPaymentReturn($params)
-	{
-		if (!$this->active)
-			return ;
-		if (substr(_PS_VERSION_, 0, 3) == '1.4')
-		{
-			global $smarty;
-			$state = $params['objOrder']->getCurrentState();
-			if ($state == Configuration::get('PS_OS_EFECTY') OR $state == Configuration::get('PS_OS_OUTOFSTOCK'))
-				$smarty->assign(array(
-					'total_to_pay' => Tools::displayPrice($params['total_to_pay'], $params['currencyObj'], false),
-					'bankwireDetails' => nl2br2($this->details),
-					'bankwireOwner' => $this->owner,
-					'status' => 'ok',
-					'id_order' => $params['objOrder']->id
-				));
-			else
-				$smarty->assign('status', 'failed');
-		}
-		if (substr(_PS_VERSION_, 0, 3) == '1.5')
-		{
-			$state = $params['objOrder']->getCurrentState();
-			if ($state == Configuration::get('PS_OS_EFECTY') || $state == Configuration::get('PS_OS_OUTOFSTOCK'))
-			{
-				$this->smarty->assign(array(
-					'total_to_pay' => Tools::displayPrice($params['total_to_pay'], $params['currencyObj'], false),
-					'efectydetails' => Tools::nl2br($this->details),
-					'efectyowner' => $this->owner,
-					'status' => 'ok',
-					'id_order' => $params['objOrder']->id
-				));
-				if (isset($params['objOrder']->reference) && !empty($params['objOrder']->reference))
-					$this->smarty->assign('reference', $params['objOrder']->reference);
-			}
-			else
-				$this->smarty->assign('status', 'failed');
-		}
-		return $this->display(__FILE__, 'payment_return.tpl');
+		return array(
+			'EFECTY_NAME' => Tools::getValue('EFECTY_NAME', Configuration::get('EFECTY_NAME')),
+			'EFECTY_DETAILS' => Tools::getValue('EFECTY_DETAILS', Configuration::get('EFECTY_DETAILS')),
+		);
 	}
 
 	public function checkCurrency($cart)
@@ -246,56 +250,41 @@ class Efecty extends PaymentModule
 		return false;
 	}
 
-	private function _createOrderState()
+	public function hookPayment($params)
 	{
-		if (!Configuration::get('PS_OS_EFECTY'))
+		if (!$this->active)
+			return;
+		if (!$this->checkCurrency($params['cart']))
+			return;
+
+		$this->smarty->assign(array(
+			'this_path' => $this->_path,
+			'this_path_efecty' => $this->_path,
+			'this_path_ssl' => Tools::getShopDomainSsl(true, true).__PS_BASE_URI__.'modules/'.$this->name.'/'
+		));
+		return $this->display(__FILE__, 'payment.tpl');
+	}
+
+	public function hookPaymentReturn($params)
+	{
+		if (!$this->active)
+			return;
+
+		$state = $params['objOrder']->getCurrentState();
+		if ($state == Configuration::get('PS_OS_EFECTY') || $state == Configuration::get('PS_OS_OUTOFSTOCK'))
 		{
-			$orderState = new OrderState();
-			$orderState->name = array();
-
-			foreach (Language::getLanguages() as $language)
-			{
-				if (strtolower($language['iso_code']) == 'es')
-					$orderState->name[$language['id_lang']] = 'EN ESPERA DE PAGO POR EFECTY';
-				else
-					$orderState->name[$language['id_lang']] = 'Awaiting Efecty Payment';
-			}
-
-			$orderState->send_email = true;
-			if (substr(_PS_VERSION_, 0, 3) == '1.4')
-				$orderState->color = 'lightblue';
-			else
-				$orderState->color = 'royalblue';
-			$orderState->hidden = false;
-			$orderState->delivery = false;
-			$orderState->logable = false;
-			$orderState->invoice = false;
-			if (substr(_PS_VERSION_, 0, 3) == '1.4')
-				$orderState->template = array_fill(0,10,"efecty");
-			else
-				$orderState->template = 'efecty';
-			if ($orderState->add())
-			{
-				@copy(dirname(__FILE__).'/../../modules/'.$this->name.'/logo.jpg', dirname(__FILE__).'/../../img/os/'.(int)$orderState->id.'.gif');
-				@copy(dirname(__FILE__).'/../../modules/'.$this->name.'/copymails/en/efecty.html', dirname(__FILE__).'/../../mails/en/efecty.html');
-				@copy(dirname(__FILE__).'/../../modules/'.$this->name.'/copymails/en/efecty.txt', dirname(__FILE__).'/../../mails/en/efecty.txt');
-				@copy(dirname(__FILE__).'/../../modules/'.$this->name.'/copymails/es/efecty.html', dirname(__FILE__).'/../../mails/es/efecty.html');
-				@copy(dirname(__FILE__).'/../../modules/'.$this->name.'/copymails/es/efecty.txt', dirname(__FILE__).'/../../mails/es/efecty.txt');
-				Configuration::updateValue('PS_OS_EFECTY', (int)$orderState->id);
-			}
+			$this->smarty->assign(array(
+				'total_to_pay' => Tools::displayPrice($params['total_to_pay'], $params['currencyObj'], false),
+				'efecty_name' => $this->efecty_name,
+				'efecty_details' => Tools::nl2br($this->efecty_details),
+				'status' => 'ok',
+				'id_order' => $params['objOrder']->id
+			));
+			if (isset($params['objOrder']->reference) && !empty($params['objOrder']->reference))
+				$this->smarty->assign('reference', $params['objOrder']->reference);
 		}
-	}
-	private function _deleteOrderState()
-	{
-		$orderState = new OrderState(Configuration::get('PS_OS_EFECTY'));
-		return $orderState->delete();	
-	}
-	
-	private function _deleteMails()
-	{
-		@unlink(dirname(__FILE__).'/../../mails/en/efecty.html');
-		@unlink(dirname(__FILE__).'/../../mails/en/efecty.txt');
-		@unlink(dirname(__FILE__).'/../../mails/es/efecty.html');
-		@unlink(dirname(__FILE__).'/../../mails/es/efecty.txt');
+		else
+			$this->smarty->assign('status', 'failed');
+		return $this->display(__FILE__, 'payment_return.tpl');
 	}
 }
